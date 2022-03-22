7bcd1d250916c00228e984764b555634c23b0ecc
Malachi Eovaldi <eova9764@kettering.edu>
2022-03-22 12:04:34 -0400

    Added ability to check off or remove tasks

--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
 import os
 import tkinter as tk
 
+from configmenu import ConfigMenu
 from consts import *
 from keyframe import Keyframe
+from setting import Setting
 
 class Tasks(tk.Label):
 
@@ -11,6 +13,8 @@ class Tasks(tk.Label):
 
         self.config(bg=BGCOL, fg=FGCOL, font=FONT)
         self.config(highlightbackground=BGCOL, highlightcolor=BGCOL, highlightthickness=BRDRWID)
+
+        self.settings = {}
 
         self.label = tk.Label(self, text='TODO')
         self.label.config(bg=BGCOL, fg=FGCOL, font=FONT_M)
@@ -23,35 +27,54 @@ class Tasks(tk.Label):
         self.update()
 
     def update(self):
+        # Create task file if it is missing
         if not os.path.exists('tasks.txt'):
             with open('tasks.txt', 'w') as tasks:
                 tasks.write('')
 
-        with open('tasks.txt') as taskfile:
+        # Read all tasks from task file
+        with open('tasks.txt', 'r') as taskfile:
             self.tasks = taskfile.readlines()
-            self.task_count = len(self.tasks)
-            self.tasks_text.config(text=''.join(self.tasks))
+
+        # Add any new tasks into settings
+        for task in self.tasks:
+            if task not in self.settings:
+                self.settings[task] = Setting(task, ['Done', 'Todo', 'Remove'], current_val='Todo')
+
+        # Format into string for display
+        taskstr = ''
+        delflag = False
+        deltasks = []
+        for task in self.settings:
+            if self.settings[task].get_value() == 'Done':
+                taskstr += f'✓ {task}\n'
+            elif self.settings[task].get_value() == 'Remove':
+                delflag = True
+                deltasks.append(self.settings[task])
+            else:
+                taskstr += f'{task}\n'
+
+        # Remove tasks from settings after iteration is complete so size doesn't change during
+        for task in deltasks:
+            print(f'Deleting task {task.get_name()}')
+            del self.settings[task.get_name()]
+        
+        # Rewrite task file without deleted tasks
+        if delflag:
+            with open('tasks.txt', 'w') as wf:
+                for task in self.settings:
+                    wf.write(task)
+
+        self.tasks_text.config(text=taskstr)
 
 
         self.after(1000, self.update)
 
-    def get_settings_menu(self, _, exiting=False):
-        return None
-        #return TasksMenu(self, columns=1, rows=self.task_count+1, tasks=self.tasks)
+    def get_settings_menu(self, root, exiting=False):
+        return ConfigMenu(root, self, exiting, title='Tasks todo')
 
-# TODO: fix
-class TasksMenu(Keyframe):
-    def __init__(self, root, *args, **kwargs):
-        tasks = kwargs.pop('tasks')
-        super().__init__(root, *args, **kwargs)
+    def get_settings(self):
+        return self.settings
 
-        self.title = tk.Label(text='TEST', bg=BGCOL, fg=FGCOL, font=FONT_SM)
-        self.add_widget(self.title, 0, 0, pack=True)
-
-        self.taskwidgets = []
-        for i in range(len(tasks)):
-            print('adding')
-            self.taskwidgets.append(tk.Label(self, text='test string'))
-            self.taskwidgets[-1].config(bg=BGCOL, fg=FGCOL, font=FONT_SM)
-            self.add_widget(self.taskwidgets[-1], 0, i, pack=True)
-
+    def set_settings(self, new_settings):
+        self.settings = new_settings
